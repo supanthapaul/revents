bc41be04bef242d5b33abe087a3a13a7905e95cf
Supantha Paul <gameover7853@gmail.com>
2018-06-18 21:22:14 +0530

    Static version with boilerplate data in react

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react'
+import ExpenseDashboard from '../../features/event/EventDashboard/EventDashboard';
+import Navbar from '../../features/nav/Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-vents</h1>
+        <Navbar />
+        <Container className="main">
+          <ExpenseDashboard />
+        </Container>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+function render() {
+   const root = document.getElementById('root');
+   ReactDOM.render(<App />, root);
+}
+
+render();
 registerServiceWorker();
