1a23816ffe35c1d91e60b22b1328d46a5a325240
Supantha Paul <gameover7853@gmail.com>
2018-06-25 13:55:29 +0530

    Setup Routing for whole application

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
 import { Container } from 'semantic-ui-react'
-import ExpenseDashboard from '../../features/event/EventDashboard/EventDashboard';
 import Navbar from '../../features/nav/Navbar/Navbar';
+import HomePage from '../../features/home/HomePage';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import EventForm from '../../features/event/EventForm/EventForm';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <Container className="main">
-          <ExpenseDashboard />
-        </Container>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+
+        <Route path='/(.+)' render={() => (
+          <div>
+            <Navbar />
+            <Container className="main">
+              <Switch>
+                <Route path="/events" component={EventDashboard} />
+                <Route path="/event/:id" component={EventDetailedPage} />
+                <Route path="/people" component={PeopleDashboard} />
+                <Route path="/profile/:id" component={UserDetailedPage} />
+                <Route path="/settings" component={SettingsDashboard} />
+                <Route path="/createEvent" component={EventForm} />
+              </Switch>
+            </Container>
+          </div>
+        )} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
@@ -8,7 +9,11 @@ import registerServiceWorker from './registerServiceWorker';
 
 function render() {
    const root = document.getElementById('root');
-   ReactDOM.render(<App />, root);
+   ReactDOM.render(
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+      , root);
 }
 
 render();
